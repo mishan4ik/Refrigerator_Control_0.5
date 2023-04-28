0fbf6a48cea65c4748204f9ad6d0be31757bc922
mishan4ik <60795949+mishan4ik@users.noreply.github.com>
2023-04-28 12:10:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let Temp = document.querySelector(".block_temperature");
+let Freezing = document.querySelector(".block_freezing");
+let cooling = document.querySelector(".block_cooling");
+Temp.onclick = function(){
+
+    window.location = "./temperature.html"
+
+}
+Freezing.onclick = function(){
+
+    window.location = "./freezing.html"
+
+}
+cooling.onclick = function(){
+
+    window.location = "./Super_cool.html"
+
+}
